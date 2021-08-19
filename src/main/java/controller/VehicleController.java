dd4345e9062074df45b92d4ec8ec507f62725a8a
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 12:38:41 +0100

    MC: testing on localhost failed.  problem solving time

--- src/main/java/controller/VehicleController.java
+++ src/main/java/controller/VehicleController.java
@@ -1,21 +1,25 @@
 package controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import service.VehicleService;
 
-@RestController	
+@RestController
 @CrossOrigin
 public class VehicleController {
-	
+
 	private VehicleService service;
 
 	public VehicleController(VehicleService service) {
 		super();
 		this.service = service;
 	}
-	
 
-	
+	@GetMapping("/hello")
+	public String helloTest() {
+		return "Hello Test";
+	}
+
 }
--- src/main/java/service/VehicleService.java
+++ src/main/java/service/VehicleService.java
@@ -1,5 +1,10 @@
 package service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class VehicleService {
 
 }
+
+// 12 mins mark on controller element of project videos
