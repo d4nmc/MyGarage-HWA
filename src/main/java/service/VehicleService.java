474a909f7648217030c83d86d7490a53ea71d9f7
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 15:04:40 +0100

    MC: still need help with localhost, added repo parts into service and interface created

--- src/main/java/service/VehicleService.java
+++ src/main/java/service/VehicleService.java
@@ -1,10 +1,36 @@
 package service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import model.Vehicle;
+import repo.VehicleRepo;
 
 @Service
 public class VehicleService {
+	
+	private VehicleRepo repo;
+	
+	public VehicleService(VehicleRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	public List<Vehicle> getAllVehicles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vehicle updateVehicle(Long id, Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteVehicle(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
 
-// 26 mins mark on controller element of project videos
