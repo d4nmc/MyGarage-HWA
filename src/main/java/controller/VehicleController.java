dbafe83411499154ab83dea25c575604f455cb88
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 12:18:27 +0100

    MC: created service class and package

--- src/main/java/controller/VehicleController.java
+++ src/main/java/controller/VehicleController.java
@@ -3,10 +3,19 @@ package controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import service.VehicleService;
+
 @RestController	
 @CrossOrigin
 public class VehicleController {
 	
+	private VehicleService service;
+
+	public VehicleController(VehicleService service) {
+		super();
+		this.service = service;
+	}
 	
 
+	
 }
