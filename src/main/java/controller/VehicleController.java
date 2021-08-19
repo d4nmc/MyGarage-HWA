b2113742edf775e39aabb34e054eabdf0fcff7de
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 12:53:03 +0100

    MC: adding in CRUD, issue with localhost still present

--- src/main/java/controller/VehicleController.java
+++ src/main/java/controller/VehicleController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import model.Vehicle;
 import service.VehicleService;
 
 @RestController
@@ -21,5 +25,13 @@ public class VehicleController {
 	public String helloTest() {
 		return "Hello Test";
 	}
+	
+	public ResponseEntity<String> createVehicle(@RequestBody Vehicle vehicle) {
+		
+		this.service.createVehicle(vehicle);
+		return new ResponseEntity<String>("Vehicle name: " + vehicle.getBrand() + vehicle.getName() + " added to list", HttpStatus.CREATED);
+	}
 
+	
+	
 }
