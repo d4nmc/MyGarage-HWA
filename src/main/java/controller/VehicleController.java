c0752666bf226b8ce875402aae5b50ecd00ec78b
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 13:22:03 +0100

    MC: adding in CRUD, issue with localhost still present

--- src/main/java/controller/VehicleController.java
+++ src/main/java/controller/VehicleController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,18 +30,32 @@ public class VehicleController {
 	public String helloTest() {
 		return "Hello Test";
 	}
-	
+
 	@PostMapping("/add")
 	public ResponseEntity<String> createVehicle(@RequestBody Vehicle vehicle) {
-		
-		// this.service.createVehicle(vehicle);
-		return new ResponseEntity<String>("Vehicle name: " + vehicle.getBrand() + vehicle.getName() + " added to garage list", HttpStatus.CREATED);
-	}
 
+		// this.service.createVehicle(vehicle);
+		return new ResponseEntity<String>(
+				"Vehicle name: " + vehicle.getBrand() + vehicle.getName() + " added to garage list",
+				HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getAll")
 	public ResponseEntity<List<Vehicle>> getAllVehicles() {
+
+		List<Vehicle> vehicleList = this.service.getAllVehicles();
+		return ResponseEntity.ok(vehicleList);
+
+}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteVehicle(@PathVariable Long id){
 		
+		this.service.deleteVehicle(id);
 		
+		return null;
 		
 	}
+	
+	
 	
 }
